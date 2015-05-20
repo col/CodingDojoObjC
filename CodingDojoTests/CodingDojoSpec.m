aca3168df7fe6d04fe3ed6092125ef2c799418d5
Colin Harris <col.w.harris@gmail.com>
2015-05-20 08:25:15 +0800

    Setup cocoapods to install Specta, Expecta and OCMock testing frameworks.

---
+++ CodingDojoTests/CodingDojoSpec.m
@@ -0,0 +1,22 @@
+//
+//  CodingDojoSpec.m
+//  CodingDojo
+//
+//  Created by Colin Harris on 20/5/15.
+//  Copyright (c) 2015 Colin Harris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Specta/Specta.h>
+
+SpecBegin(Thing)
+
+describe(@"Thing", ^{
+    
+    it(@"should do stuff", ^{
+        // This is an example block. Place your assertions here.
+    });
+    
+});
+
+SpecEnd
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'CodingDojo' do
+
+end
+
+target 'CodingDojoTests' do
+  pod 'Expecta', '~> 0.3.2'
+  pod 'Specta',  '~> 0.3.2'
+  pod 'OCMock',  '~> 3.1.2'  
+end
+
